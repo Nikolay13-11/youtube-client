56034c290f3eb5f70e99d28148f6b84f663ac82a
Nikolay13-11 <mikola13.11@mail.ru>
2021-08-15 11:03:24 +0300

    Start generate app structure

---
+++ youtube-client/src/app/youtobe/youtobe.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { YoutobeRoutingModule } from './youtobe-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    YoutobeRoutingModule
+  ]
+})
+export class YoutobeModule { }
